4d8590d9283f29601f24d3851e026662d1b4b706
Caleb <belac335@gmail.com>
2021-03-02 15:51:01 +0100

    setup project

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <form>
+      <div>
+        <label htmlFor="fullname">Full name</label>
+        <input type="text" placeholder="enter your fullname" />
+      </div>
+    </form>
+  );
+};
+
+export default App;
